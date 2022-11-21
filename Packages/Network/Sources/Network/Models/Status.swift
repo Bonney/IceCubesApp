22b0afab8609f2b17979f7fd1d88a1d68c64563e
Matt Bonney <bonneym@gmail.com>
2022-11-21 18:47:11 -0500

    for Xcode previews

--- Packages/Network/Sources/Network/Models/Status.swift
+++ Packages/Network/Sources/Network/Models/Status.swift
@@ -5,3 +5,8 @@ public struct Status: Codable, Identifiable {
   public let content: String
   public let account: Account
 }
+
+// MARK: Preview Content
+public extension Status {
+    static let preview: Status = Status(id: "0987654321", content: "This is my very special Mastodon post with lots of very insightful and thoughtful things to say.", account: Account.preview)
+}
